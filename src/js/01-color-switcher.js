6ffb8cf8da5f391136a263209750e1ee9bafd8a9
AnastasiiaVld <anastasiia94cherniavska@gmail.com>
2023-04-03 22:04:28 +0200

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onBtnClickStart() {
+  timerId = setInterval(() => {
+    bodyEl.style.background = getRandomHexColor();
+  }, 1000);
+
+  if (timerId) {
+    startBtn.disabled = true;
+  }
+}
+
+function onBtnClickStop() {
+  clearInterval(timerId);
+  if (timerId) {
+    startBtn.disabled = false;
+  }
+}
+
+startBtn.addEventListener('click', onBtnClickStart);
+stopBtn.addEventListener('click', onBtnClickStop);
