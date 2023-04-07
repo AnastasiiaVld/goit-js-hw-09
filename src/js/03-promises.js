0e155524713e682f3e1b87ee49a1df7e0e56da20
AnastasiiaVld <anastasiia94cherniavska@gmail.com>
2023-04-07 16:38:53 +0200

    replace 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -18,9 +18,12 @@ function onsubmitBtn(e) {
 
   for (let i = 0; i < amount; i++) {
     const newDelay = step * i + firstStep;
-    createPromise(i + 1, newDelay)
-      .then(data => Notiflix.Notify.success(data))
-      .catch(error => Notiflix.Notify.failure(error));
+
+    setTimeout(() => {
+      createPromise(i + 1, newDelay)
+        .then(data => Notiflix.Notify.success(data))
+        .catch(error => Notiflix.Notify.failure(error));
+    }, newDelay);
   }
 }
 
@@ -28,14 +31,54 @@ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      } else {
-        reject(`❌ Rejected promise ${position} in ${delay}ms`);
-      }
-    }, step);
+    if (shouldResolve) {
+      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
   });
 }
 
 formEl.addEventListener('submit', onsubmitBtn);
+
+// import Notiflix from 'notiflix';
+
+// const formEl = document.querySelector('.form');
+// const formDelay = document.querySelector('[name="delay"]');
+// const formStep = document.querySelector('[name="step"]');
+// const formAmount = document.querySelector('[name="amount"]');
+
+// let amount = 0;
+// let step = 0;
+// let firstStep = 0;
+
+// function onsubmitBtn(e) {
+//   e.preventDefault();
+
+//   firstStep = +formDelay.value;
+//   step = +formStep.value;
+//   amount = +formAmount.value;
+
+//   for (let i = 0; i < amount; i++) {
+//     const newDelay = step * i + firstStep;
+//     createPromise(i + 1, newDelay)
+//       .then(data => Notiflix.Notify.success(data))
+//       .catch(error => Notiflix.Notify.failure(error));
+//   }
+// }
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       } else {
+//         reject(`❌ Rejected promise ${position} in ${delay}ms`);
+//       }
+//     });
+//   });
+// }
+
+// formEl.addEventListener('submit', onsubmitBtn);
